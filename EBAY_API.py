9a0ccecc1e1dd07871ddf7b804289427f0e97d57
Santayo2004 <sflorezr@uwo.ca>
2024-03-22 11:52:42 -0400

    Search for a dog food and recieves info about it (link, title, costs, cost per kg)

---
+++ EBAY_API.py
@@ -0,0 +1,195 @@
+#import requests
+#import json
+
+''' commented out because I have used 80%  + of my free credits
+# set up the request parameters
+params = {
+'api_key': 'C4B451BB5849470D930600A61640AE26',
+  'ebay_domain': 'ebay.ca',
+  'search_term': 'dog food',
+  'type': 'search',
+  'authorized_sellers': 'false',
+  'max_page': '5',
+  'sort_by': 'best_match',
+  'output': 'json',
+  'customer_location': 'ca'
+}
+
+# make the http GET request to Countdown API
+api_result = requests.get('https://api.countdownapi.com/request', params)
+'''
+
+# print the JSON response from Countdown API
+#print(json.dumps(api_result.json()))
+
+# need to find average price of brand and cost per kg
+
+#responseString = json.dumps(api_result.json())
+#print(type(responseString))
+
+#putting both strings in lower case to make search case-insensitive
+
+#product finding
+productRequest = input('Enter a product: ')
+productRequest = productRequest.lower()
+
+#setting a string to the response sent from the api
+responseString = 'random characters for simulatiotonsonwvoinvoinwoinwovinwroinvrwoinofivneroin{"position": 37, "title": "Retriever High-Protein Adult Performance Beef & Chicken Dry Dog Food - 654/50kg Bag", "epid": "134742825105", "link": "https://www.ebay.ca/itm/134742825105", "image": "https://i.ebayimg.com/thumbs/images/g/pCEAAOSwOS1lETEE/s-l300.jpg", "condition": "Brand New", "seller_info": {"name": "gregory.281", "review_count": 41808, "positive_feedback_percent": 99.9}, "is_auction": false, "buy_it_now": false, "free_returns": false, "item_location": "from United States", "shipping_cost": 285.74, "sponsored": false, "prices": [{"value": 76.59, "raw": "$76.59"}], "price": {"value": 76.59, "raw": "$76.59"}, "page": 2, "position_overall": 97},' 
+responseStringLower = responseString.lower()
+
+#searching the response to find searched dog food
+search = responseStringLower.find(productRequest)
+
+#if the search finds nothing
+if search == -1:
+  print('This dog food was not found in the search')
+
+#if the search finds something
+else: 
+  #finding the whole title of the dog food
+  titlePos = responseString.find('"title": ', search - 40) + 10
+  endTitle = responseString.find('", "epid": ', search)
+  title = responseString[titlePos: endTitle]
+  
+  #finding the price
+  moneyPos = responseString.find('$', search) 
+  price = responseString[moneyPos + 1: responseString.find('"}], "price": ', search)] 
+  price = float(price)
+  
+  #finding the link
+  linkPos = responseString.find('https', search)
+  link = responseString[linkPos : responseString.find('", "image": ', search)]
+  
+  #finding the shipping cost
+  shippingPos = responseString.find('"shipping_cost": ')
+  shippingCost = responseString[shippingPos + 17 : responseString.find(', "sponsored": ', search)]
+  shippingCost = float(shippingCost)
+  
+  totalCost = round(price + shippingCost, 2)
+
+  #printing results
+  print('\n' + title)
+  print('Price: $' + str(price))
+  print('Shipping Cost: $' + str(shippingCost))
+  print('Total cost: $' + str(totalCost))
+  print('found at: ' + link)
+  
+  #finding the weight
+  
+  #finding the position of the weight unit, if there is one
+  kgPos = title.lower().find('kg')
+  lbPos = title.lower().find('lb')
+  ozPos = title.lower().find('oz')
+  poundPos = title.lower().find('pounds')
+  canPos = title.lower().find('can')
+  
+  units = {1: kgPos, 2: lbPos, 3: ozPos,4 : poundPos, 5 : 5}
+  
+ 
+  #using a reversed to string to find the numbers nearest to the end  
+  reverseTitle = title [::-1]
+  
+  #finding the positions of each digit 0-9
+  numberPositions = []
+  for x in range(10):
+    numberPositions.append(len(title) - reverseTitle.find(str(x)) - 1)
+    
+    #if not found, it has position -1
+    if numberPositions[x] == len(title):
+      numberPositions[x] = -1
+    
+  
+  #making a list to find only the positions of existing digits in the 
+  existingNumberPositions = []
+  for x in range(10):
+    if numberPositions[x] != -1:
+      existingNumberPositions.append(numberPositions[x])
+      
+  if len(existingNumberPositions) == 0:
+    existingNumberPositions.append(0)
+    
+  gPos = title.lower().find('g', title.find(str(max(existingNumberPositions))), title.find(str(max(existingNumberPositions))) + 1)
+  units.update({6: gPos}) 
+  unitInUse = max(zip(units.values(), units.keys()))[1] 
+  #print(existingNumberPositions)
+  
+  #weight reading system is very delicate 
+  
+  #making it robust to spaces and slashes
+  weightString = title[min(existingNumberPositions): max(existingNumberPositions)+1]
+  #print(weightString)
+  findingSpace = weightString.find(' ')
+  findingSlashes = weightString.find('/')
+  
+  if findingSpace != -1:
+    weightString = weightString[findingSpace + 1: len(weightString)]
+  
+  if findingSlashes != -1: 
+    weightString = weightString[findingSlashes + 1: len(weightString)]
+    
+  #print(weightString)
+  
+  #print(title[min(existingNumberPositions): max(existingNumberPositions)+1])
+  #print(existingNumberPositions)
+  
+  #weightNumber = float(title[min(existingNumberPositions): max(existingNumberPositions)+1])
+  
+  try:
+    weightNumber = float(weightString)
+
+  except ValueError:
+    print("No weight could be read")
+  
+  #if the weight reading system does work
+  else:  
+    # if there are no mentions of cans
+    if canPos == -1:
+      #depending on the units used, the cost per kg changes
+      match unitInUse: 
+        case 1:
+          pricePerKG = totalCost / weightNumber
+          print('The price per Kg is: $' + str(round(pricePerKG, 2)))
+        case 2 :
+          weightNumber = 0.453592 * weightNumber
+          pricePerKG = totalCost / weightNumber
+          print('The price per Kg is: $' + str(round(pricePerKG, 2)))
+        case 3:
+          weightNumber = 0.0283495 * weightNumber
+          pricePerKG = totalCost / weightNumber
+          print('The price per Kg is: $' + str(round(pricePerKG, 2)))
+        case 4:
+          weightNumber = 0.453592 * weightNumber
+          pricePerKG = totalCost / weightNumber
+          print('The price per Kg is: $' + str(round(pricePerKG, 2)))
+        case 5: 
+          print('there are no units')
+        case 6: 
+          weightNumber = 0.001 * weightNumber
+          pricePerKG = totalCost / weightNumber
+          print('The price per Kg is: $' + str(round(pricePerKG, 2)))
+    #if theres is a mention of can, the weight reading system doesn't work
+    else: 
+      print('weight could not be read')
+        
+  
+ 
+  '''print(unitInUse); print(weightNumber)
+  print(title.find(str(5)))
+  print (numberPositions)
+  print(lbPos)
+  print(len(title))'''
+  
+  
+    
+  
+  
+  
+
+
+
+
+
+
+
+
+
